20234682f6dea98112ca5f845dfd0bae0fe1af2e
郑杰 <939783694@qq.com>
2023-09-20 00:57:30 +0800

    fix: 页面管理

--- src/modules/sys/views/views.controller.ts
+++ src/modules/sys/views/views.controller.ts
@@ -1,34 +1,50 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+/*
+ * @Author: ZhengJie
+ * @Date: 2023-08-29 00:00:54
+ * @Description: controller.views
+ */
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ViewsService } from './views.service';
 import { CreateViewDto } from './dto/create-view.dto';
 import { UpdateViewDto } from './dto/update-view.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { getControllerName } from 'src/utils';
 
-@Controller('views')
+@ApiTags('页面管理')
+@Controller(getControllerName(__dirname))
 export class ViewsController {
   constructor(private readonly viewsService: ViewsService) {}
 
-  @Post()
-  create(@Body() createViewDto: CreateViewDto) {
-    return this.viewsService.create(createViewDto);
-  }
+  // @Post()
+  // create(@Body() createViewDto: CreateViewDto) {
+  //   return this.viewsService.create(createViewDto);
+  // }
 
-  @Get()
-  findAll() {
-    return this.viewsService.findAll();
-  }
+  // @Get()
+  // findAll() {
+  //   return this.viewsService.findAll();
+  // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.viewsService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.viewsService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateViewDto: UpdateViewDto) {
-    return this.viewsService.update(+id, updateViewDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateViewDto: UpdateViewDto) {
+  //   return this.viewsService.update(+id, updateViewDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.viewsService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.viewsService.remove(+id);
+  // }
 }
