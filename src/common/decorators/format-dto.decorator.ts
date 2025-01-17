512638d9e08b1f9346201b3364b77f5b85c2145f
郑杰 <939783694@qq.com>
2025-01-18 01:35:40 +0800

    feat: 新增“格式化参数”相关方法装饰器

--- src/common/decorators/format-dto.decorator.ts
+++ src/common/decorators/format-dto.decorator.ts
@@ -32,3 +32,52 @@ export const FormatDtoEmpty = createParamDecorator(
     return result;
   },
 );
+
+/**
+ * 格式化分页参数
+ */
+export const FormatDefaultPagination = (): MethodDecorator => {
+  return (_target, _propertyKey, descriptor: PropertyDescriptor) => {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = async function (...args: any[]) {
+      if (args[0] && typeof args[0] === 'object') {
+        const dto = args[0];
+        args[0] = {
+          ...dto,
+          pageNo: dto.pageNo ? +dto.pageNo : 1,
+          pageSize: dto.pageSize ? +dto.pageSize : 15,
+          status: dto.status || '0',
+        };
+      }
+      return originalMethod.apply(this, args);
+    };
+    return descriptor;
+  };
+};
+
+/**
+ * 去除参数中为空字符串或者null的参数
+ */
+export const FormatEmptyParams = (): MethodDecorator => {
+  return (_target, _propertyKey, descriptor: PropertyDescriptor) => {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = async function (...args: any[]) {
+      if (args[0] && typeof args[0] === 'object') {
+        const targetParams = {};
+        const dto = args[0];
+        for (const item in dto) {
+          if (dto[item] !== '' && dto[item] !== null) {
+            targetParams[item] = dto[item];
+          }
+        }
+        args[0] = {
+          ...targetParams,
+        };
+      }
+      return originalMethod.apply(this, args);
+    };
+    return descriptor;
+  };
+};
