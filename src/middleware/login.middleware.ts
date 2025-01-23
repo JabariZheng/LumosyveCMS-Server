b54e6523340c5b64089beb9b0f8cca2bc5220280
郑杰 <939783694@qq.com>
2025-01-24 02:45:10 +0800

    fix: 登录校验中间层

--- src/middleware/login.middleware.ts
+++ src/middleware/login.middleware.ts
@@ -3,12 +3,56 @@
  * @Date: 2023-07-28 14:31:29
  * @Description: middleware.login
  */
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import {
+  forwardRef,
+  Inject,
+  Injectable,
+  NestMiddleware,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { CacheService } from 'src/modules/cache/cache.service';
+import { AuthService } from 'src/modules/sys/auth/auth.service';
 
 @Injectable()
 export class LoginMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: (error?: any) => void) {
-    // console.log('is login valid');
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly cacheService: CacheService,
+    @Inject(forwardRef(() => AuthService))
+    private readonly authService: AuthService,
+  ) {}
+  async use(req: any, res: any, next: (error?: any) => void) {
+    const WHITE_LIST = this.configService.get('common.whiteList');
+    if (WHITE_LIST.includes(req.url)) {
+      next();
+      return;
+    }
+    const isPermission = await this.configService.get('app.permission');
+    const authorization = req.headers.authorization;
+
+    if (
+      isPermission &&
+      (!authorization || authorization.split('Bearer ').length < 2)
+    ) {
+      throw new UnauthorizedException('请先登录');
+    }
+
+    // 解token
+    const { status, data, msg } = await this.authService.validToken(
+      authorization.replace('Bearer ', ''),
+    );
+    if (status === 200) {
+      // redis查询是否存在有效登录
+      const cacheData = await this.cacheService.get(
+        `user_${data.id}_${data.userName}`,
+      );
+      if (!cacheData) {
+        throw new UnauthorizedException('请先登录');
+      }
+    } else {
+      throw new UnauthorizedException(msg);
+    }
     next();
   }
 }
