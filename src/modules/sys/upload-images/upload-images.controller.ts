f6c830db9313edc138efeea43c1db7bbabbb4f50
郑杰 <939783694@qq.com>
2025-01-24 02:43:10 +0800

    fix: 图片上传

--- src/modules/sys/upload-images/upload-images.controller.ts
+++ src/modules/sys/upload-images/upload-images.controller.ts
@@ -3,26 +3,15 @@
  * @Date: 2023-10-28 22:03:56
  * @Description: uploadImages.controller
  */
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  UploadedFile,
-  UploadedFiles,
-} from '@nestjs/common';
+import { Controller, Post, UploadedFile, UploadedFiles } from '@nestjs/common';
 import { UploadImagesService } from './upload-images.service';
-import { CreateUploadImageDto } from './dto/create-upload-image.dto';
-import { UpdateUploadImageDto } from './dto/update-upload-image.dto';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import {
   UploadImg,
   UploadImgs,
 } from 'src/common/decorators/upload-img.decorator';
 import { ResultData } from 'src/utils/result';
+import { CatchErrors } from 'src/common/decorators/catch-error.decorator';
 
 @ApiTags('图片上传')
 @Controller('/sys/upload-images')
@@ -32,10 +21,13 @@ export class UploadImagesController {
   @Post('')
   @UploadImgs()
   @ApiOperation({ summary: '上传图片多个' })
-  uploadImages(@UploadedFiles() files: { file: Express.Multer.File[] }) {
+  @CatchErrors()
+  uploadImages(@UploadedFiles() files: { files: Express.Multer.File[] }) {
     const { host, port } = this.uploadImagesService.getAppServer();
-    const result = files.file.map((file) => {
+    const result = files.files.map((file) => {
       return {
+        name: file.filename,
+        size: file.size,
         url: `http://${host}:${port}/${file.path}`,
       };
     });
@@ -45,9 +37,12 @@ export class UploadImagesController {
   @Post('item')
   @UploadImg()
   @ApiOperation({ summary: '上传图片单个' })
+  @CatchErrors()
   uploadImageItem(@UploadedFile() file: Express.Multer.File) {
     const { host, port } = this.uploadImagesService.getAppServer();
     return ResultData.ok({
+      name: file.filename,
+      size: file.size,
       url: `http://${host}:${port}/${file.path}`,
     });
   }
